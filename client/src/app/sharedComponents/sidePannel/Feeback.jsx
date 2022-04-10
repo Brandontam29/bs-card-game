7f4953ee334f6d1e201c9d7d7d887cc4ff5a8d73
Brandon Tam <brandon.tam29@gmail.com>
2022-04-10 19:17:05 -0400

    added clear form

--- client/src/app/sharedComponents/sidePannel/Feeback.jsx
+++ client/src/app/sharedComponents/sidePannel/Feeback.jsx
@@ -16,16 +16,22 @@ const defaultProps = {};
 
 const Feedback = ({ closePannel }) => {
     const [email, setEmail] = useState('');
-    const [body, setBody] = useState('');
+    const [comment, setComment] = useState('');
     const [rating, setRating] = useState(0);
     const [submitted, setSubmitted] = useState(false);
 
     const handleSubmit = () => {
         setSubmitted(true);
-        console.log(email, body, rating, submitted);
         // const id = setTimeout(() => setSubmitted(false), 3);
         // setPannelOpen(false);
         // return clearTimeout(id);
+    };
+
+    const handleCancel = () => {
+        setEmail('');
+        setComment('');
+        setRating(0);
+        closePannel();
     };
 
     return (
@@ -61,20 +67,32 @@ const Feedback = ({ closePannel }) => {
                                 className="border rounded w-4/5 p-1"
                             />
                         </label>
-                        <Ratings onChange={setRating} />
+                        <Ratings rating={rating} onChange={setRating} />
                         <label htmlFor="comments">
                             <div>Comments</div>
                             <textarea
                                 id="comments"
-                                value={body}
-                                onChange={(e) => setBody(e.target.value)}
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
                                 className="border rounded w-full min-h-[100px] p-1"
                             />
                         </label>
-
-                        <button type="button" onClick={handleSubmit}>
-                            Submit
-                        </button>
+                        <div className="flex flex-row justify-end gap-2">
+                            <button
+                                type="button"
+                                onClick={handleCancel}
+                                className="border border-solid border-gray-400 text-gray-500 rounded py-1 px-2"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="bg-blue-500 rounded py-1 px-2 text-white"
+                            >
+                                Submit
+                            </button>
+                        </div>
                     </form>
                 </>
             )}
@@ -86,7 +104,7 @@ Feedback.propTypes = propTypes;
 Feedback.defaultProps = defaultProps;
 
 const WithReduxContainer = connect(
-    ({ site }) => ({}),
+    () => ({}),
     (dispatch) => ({
         setPannelOpen: (value) => dispatch(setPannelOpenAction(value)),
     }),
--- client/src/app/sharedComponents/sidePannel/Ratings.jsx
+++ client/src/app/sharedComponents/sidePannel/Ratings.jsx
@@ -1,30 +1,69 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { PropTypes } from 'prop-types';
 import styles from './ratings.module.css';
 
 const propTypes = {
+    rating: PropTypes.bool.isRequired,
     onChange: PropTypes.func.isRequired,
 };
 
 const defaultProps = {};
 
-const Ratings = ({ onChange }) => {
+const Ratings = ({ rating, onChange }) => {
     const onClick = (e) => {
-        e.preventDefault();
         onChange(e.target.value);
     };
 
+    console.log(rating);
+
     return (
         <div className={styles.rating}>
-            <input type="radio" name="rating" value="5" id="5" onChange={onClick} />
-            <label htmlFor="5">☆</label>
-            <input type="radio" name="rating" value="4" id="4" onChange={onClick} />
-            <label htmlFor="4">☆</label>
-            <input type="radio" name="rating" value="3" id="3" onChange={onClick} />
-            <label htmlFor="3">☆</label>
-            <input type="radio" name="rating" value="2" id="2" onChange={onClick} />
-            <label htmlFor="2">☆</label>
-            <input type="radio" name="rating" value="1" id="1" onChange={onClick} />
-            <label htmlFor="1">☆</label>
+            <input
+                type="radio"
+                id="star5"
+                name="rate"
+                value="5"
+                {...(rating === 0 && { checked: false })}
+                onChange={onClick}
+            />
+            <label htmlFor="star5" title="text" />
+            <input
+                type="radio"
+                id="star4"
+                name="rate"
+                value="4"
+                {...(rating === 0 && { checked: false })}
+                onChange={onClick}
+            />
+            <label htmlFor="star4" title="text" />
+            <input
+                type="radio"
+                id="star3"
+                name="rate"
+                value="3"
+                {...(rating === 0 && { checked: false })}
+                onChange={onClick}
+            />
+            <label htmlFor="star3" title="text" />
+            <input
+                type="radio"
+                id="star2"
+                name="rate"
+                value="2"
+                {...(rating === 0 && { checked: false })}
+                onChange={onClick}
+            />
+            <label htmlFor="star2" title="text" />
+            <input
+                type="radio"
+                id="star1"
+                name="rate"
+                value="1"
+                {...(rating === 0 && { checked: false })}
+                onChange={onClick}
+            />
+            <label htmlFor="star1" title="text" />
         </div>
     );
 };
